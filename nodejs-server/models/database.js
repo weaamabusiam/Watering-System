4436856fe3c66141bb3539454cc3a2fb028ee689
Weaam Abu Siam <131193601+weaamabusiam@users.noreply.github.com>
2025-03-13 21:26:35 +0200

    Add files via upload

--- nodejs-server/models/database.js
+++ nodejs-server/models/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.DB_HOST,
-//     port:process.env.PORT,
+    port:process.env.DB_PORT,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 
 module.exports = pool.promise();
